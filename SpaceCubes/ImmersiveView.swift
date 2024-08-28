62e0365755c9fd5dc05892e7df53a9e2e3a1c937
Tim Mitra <tim@it-guy.com>
2024-08-28 10:46:12 -0400

    added attachment label

--- SpaceCubes/ImmersiveView.swift
+++ SpaceCubes/ImmersiveView.swift
@@ -11,12 +11,31 @@ import RealityKitContent
 
 struct ImmersiveView: View {
     var body: some View {
-        RealityView { content in
-            // Add the initial RealityKit content
-            if let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
-                content.add(scene)
+      RealityView { content, attachments  in
+        // Add the initial RealityKit content
+        if let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+          content.add(scene)
+            
+        }
+      } update: { content, attachments in
+          if let scene = content.entities.first {
+              if let cubeAttachment = attachments.entity(for: "cube-label") {
+                  cubeAttachment.position = [0, 0, -1.5]
+                  scene.addChild(cubeAttachment)
+              }
+          }
+          // ...
+      } attachments: {
+        Attachment(id: "cube-label") {
+            VStack {
+                Text("Space Cubes")
+                    .font(.title)
             }
-        }.offset(y:-1200)
+            .padding(.all, 15)
+            .glassBackgroundEffect()
+        }
+        
+      }.offset(y:-1200)
     }
 }
 
